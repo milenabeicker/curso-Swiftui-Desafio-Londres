30f83d99d1beb9b00519b733f3e0401bfbd146a2
Milena Kessia <milenakessia87@gmail.com>
2023-03-28 19:36:06 -0300

    Initial Commit

---
+++ Desafio-Londres/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Desafio-Londres
+//
+//  Created by Milena Kessia on 28/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Desafio-Londres/Desafio_LondresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_LondresApp.swift
+//  Desafio-Londres
+//
+//  Created by Milena Kessia on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Desafio_LondresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
